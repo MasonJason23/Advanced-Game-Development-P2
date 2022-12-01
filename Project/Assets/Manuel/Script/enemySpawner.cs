becc25f7cc4c18def68792a2b67c157e4ba56d79
Manchips <mansanchez@csumb.edu>
2022-12-01 13:59:34 -0800

    Fixed spawner and enemySCR
    Now they have no reliance on a ground area and solely relay on themselves, tested on old branch so these huge amounts of changes shouldnt't matter

--- Project/Assets/Manuel/Script/enemySpawner.cs
+++ Project/Assets/Manuel/Script/enemySpawner.cs
@@ -7,103 +7,104 @@ using Random = UnityEngine.Random;
 
 public class enemySpawner : MonoBehaviour
 {
-    //enemy prefab, and the object for the map
-    public GameObject enemyPrefab;
-    public GameObject gameArea;
-    public GameObject xpOrbs;
-    public Transform target;
-    public float spawnTime = 2;
-    public float spawnDelay = 3;
-    
-    //This is for testing enemy spawning
-    public int enemyCount = 0;
-    public int enemyLimit = 5;
-    public int enemyPerFrame = 1;
+   //enemy prefab, and the object for the map
+   public GameObject enemyPrefab;
+   //public GameObject gameArea;
+   public GameObject xpOrbs;
+   public Transform target;
+   public float spawnTime = 2;
+   public float spawnDelay = 3;
+  
+   //This is for testing enemy spawning
+   public int enemyCount = 0;
+   public int enemyLimit = 5;
+   public int enemyPerFrame = 1;
 
-    //The top one is for the spawning circle and the bottom is for testing, will maybe keep it(?)
-    public float spawnCircleRadius = .5f;
-    public float deathCircleRadius = 2.0f;
-    
-    //Speeds for testing 
-    public float slowestSpeed = 10f;
-    public float fastestSpeed = 1f;
+   //The top one is for the spawning circle and the bottom is for testing, will maybe keep it(?)
+   public float spawnCircleRadius = .5f;
+   public float deathCircleRadius = 2.0f;
+  
+   //Speeds for testing
+   public float slowestSpeed = 10f;
+   public float fastestSpeed = 1f;
 
-    public float enemyCost = 2; //This is for testing the directorFast
-    
-    //TODO 1: Change spawning from population based to enemies purchased 
-    //TODO 2: Make the spawning based on director i.e fast/slow 
-    
-    private void Start()
-    {
-        //InvokeRepeating("MaintainPopulation",spawnTime,spawnDelay);
-    }
+   public float enemyCost = 2; //This is for testing the directorFast
+  
+   //TODO 1: Change spawning from population based to enemies purchased
+   //TODO 2: Make the spawning based on director i.e fast/slow
+  
+   private void Start()
+   {
+       //InvokeRepeating("MaintainPopulation",spawnTime,spawnDelay);
+   }
 
-    private void Update()
-    {
-        //we check every frame to make sure we have enough enemies this will have to be changed if we want enemies spawn rate to scale with time.
-        //MaintainPopulation();
-    }
+   private void Update()
+   {
+       //we check every frame to make sure we have enough enemies this will have to be changed if we want enemies spawn rate to scale with time.
+       //MaintainPopulation();
+   }
 
-    void OnEnable()
-    {
-        directorFast.spawn += MaintainPopulation;
-    }
+   void OnEnable()
+   {
+       directorFast.spawn += MaintainPopulation;
+   }
 
-    void onDisable()
-    {
-        directorFast.spawn -= MaintainPopulation;
-    }
+   void onDisable()
+   {
+       directorFast.spawn -= MaintainPopulation;
+   }
 
-    void MaintainPopulation()
-    {
-        if (enemyCount < enemyLimit)//we check to make sure we don't go over our limit, this will probably not be needed later on
-            {
-                for (int i = 0; i < enemyPerFrame; i++)
-                {
-                    Vector3 position = GetRandomPosition(true); //we send a bool that shows if our enemy spawn positions would be in view 
-                    enemySCR enemyScript = AddEnemy(position);
-                
-                    enemyScript.transform.Rotate(Vector3.forward * Random.Range(-45.0f,45.0f)); //this moves there spawning point to be closer but may not be needed 
-                }
-            }
-    }
+   void MaintainPopulation()
+   {
+       if (enemyCount < enemyLimit)//we check to make sure we don't go over our limit, this will probably not be needed later on
+           {
+               for (int i = 0; i < enemyPerFrame; i++)
+               {
+                   Vector3 position = GetRandomPosition(true); //we send a bool that shows if our enemy spawn positions would be in view
+                   enemySCR enemyScript = AddEnemy(position);
+              
+                   enemyScript.transform.Rotate(Vector3.forward * Random.Range(-45.0f,45.0f)); //this moves there spawning point to be closer but may not be needed
+               }
+           }
+   }
 
-    Vector3 GetRandomPosition(bool withinCamera)
-    {
-        Vector3 position = Random.insideUnitCircle.normalized; //This will push enemies to the edge of the circle, later on may add some random number to move them either closer to further from the edge of the circle.
+   Vector3 GetRandomPosition(bool withinCamera)
+   {
+       Vector3 position = Random.insideUnitCircle.normalized; //This will push enemies to the edge of the circle, later on may add some random number to move them either closer to further from the edge of the circle.
 
-        //This makes there spawning be on y axis of 0 but there z position is equal to the random value we normalized 
-        position = new Vector3(position.x, 0, position.y);
-        
-        //this will be used later so enemies just spawn in the camera view, so even in fog we can spawn enimies that aren't too far away! 
-        if(withinCamera == false)
-        {
-            position = position.normalized;
-        }
+       //This makes there spawning be on y axis of 0 but there z position is equal to the random value we normalized
+       position = new Vector3(position.x, 0, position.y);
+      
+       //this will be used later so enemies just spawn in the camera view, so even in fog we can spawn enemies that aren't too far away!
+       if(withinCamera == false)
+       {
+           position = position.normalized;
+       }
 
-        position *= spawnCircleRadius; //This influences the spawning radius 
-        position += target.transform.position; //This focus 
-        
-        return position;
-    }
+       position *= spawnCircleRadius; //This influences the spawning radius
+       position += target.transform.position; //This focuses the spawn radius to be around the player, i.e player = center of circle
+      
+       return position;
+   }
 
-    enemySCR AddEnemy(Vector3 position)
-    {
-        enemyCount += 1; //we need to add on, later on this will be changed to append for the array
-        //We instantiatew a new newEnemy, this is fairly staright forward, although we could change vector3.up to something else for grounded enemies
-        GameObject newEnemy = Instantiate( 
-            enemyPrefab,
-            position,
-            Quaternion.FromToRotation(Vector3.up, (gameArea.transform.position - position)),
-            gameObject.transform);
-        
-        enemySCR enemyScript = newEnemy.GetComponent<enemySCR>(); //We give the enemySCR its elements 
-        enemyScript.enemySpawner = this; //This just gives it this script, giving them access to one another 
-        enemyScript.gameArea = gameArea; //Placing the gameArea(plane) to the enemyPrefab script
-        enemyScript.target = target; //This is also needed for the nav mesh, the target is the player prefab 
-        enemyScript.xpOrbs = xpOrbs;
-        enemyScript.speed = Random.Range(slowestSpeed, fastestSpeed); //This is the speed range although its probably too fast rn 
+   enemySCR AddEnemy(Vector3 position)
+   {
+       enemyCount += 1; //we need to add on, later on this will be changed to append for the array
+       //We instantiate a new newEnemy, this is fairly staright forward, although we could change vector3.up to something else for grounded enemies
+       GameObject newEnemy = Instantiate(
+           enemyPrefab,
+           position,
+           Quaternion.FromToRotation(Vector3.up, (this.transform.position - position)),
+           gameObject.transform);
+      
+       enemySCR enemyScript = newEnemy.GetComponent<enemySCR>(); //We give the enemySCR its elements
+       enemyScript.enemySpawner = this; //This just gives it this script, giving them access to one another
+       //enemyScript.gameArea = gameArea; //Placing the gameArea(plane) to the enemyPrefab script NOT NEEDED
+       enemyScript.target = target; //This is also needed for the nav mesh, the target is the player prefab
+       enemyScript.xpOrbs = xpOrbs;
+       enemyScript.speed = Random.Range(slowestSpeed, fastestSpeed); //This is the speed range although its probably too fast rn
 
-        return enemyScript;
-    }
+       return enemyScript;
+   }
 }
+
